3c80d58c2f7eaa03697e7ea7833aaae46d7bfb25
Anil Sharma <anil@propriocept.com>
2023-10-07 14:54:34 +0200

    Black and ruff cleanup

--- src/seb_to_goodbudget/main.py
+++ src/seb_to_goodbudget/main.py
@@ -1,19 +1,17 @@
 """One file for all functions to begin with."""
-import os
 
-from datetime import datetime
 from decimal import Decimal
 import pandas as pd
 from pathlib import Path
-from typing import Union
+
 
 def read_excel(filename: str | Path) -> pd.DataFrame:
     """Reads an .xlsx file from SEB and returns transactions as a pandas dataframe.
-    
+
     Parameters
     ----------
     filename : str, pathlib.Path
-    
+
     Returns
     -------
     pandas.DataFrame
@@ -22,12 +20,16 @@ def read_excel(filename: str | Path) -> pd.DataFrame:
             Name : str
             Amount : Decimal
     """
-    in_df = pd.read_excel(io=filename, sheet_name="Sheet1", skiprows=7, header=0, dtype={"Belopp": float})
-    out_df = pd.DataFrame({
-        "Date": pd.to_datetime(in_df["Bokförd"]),
-        "Name": in_df["Text"],
-        "Amount": in_df["Belopp"],
-    })
+    in_df = pd.read_excel(
+        io=filename, sheet_name="Sheet1", skiprows=7, header=0, dtype={"Belopp": float}
+    )
+    out_df = pd.DataFrame(
+        {
+            "Date": pd.to_datetime(in_df["Bokförd"]),
+            "Name": in_df["Text"],
+            "Amount": in_df["Belopp"],
+        }
+    )
     # Convert Amount to decimals because they are currency.
     out_df["Amount"] = [Decimal(x) for x in out_df.Amount]
     return out_df
@@ -35,6 +37,7 @@ def read_excel(filename: str | Path) -> pd.DataFrame:
 
 def main() -> bool:
     return True
+
 
 if __name__ == "main":
     main()
--- tests/test_main.py
+++ tests/test_main.py
@@ -5,24 +5,40 @@ from decimal import Decimal
 import pandas as pd
 from pathlib import Path
 import pytest
-from typing import Generator
 
 import seb_to_goodbudget.main as M
 
-fixture_input_filenames = ["tests/fixtures/test_input.xlsx", Path("tests/fixtures/test_input.xlsx")]
+fixture_input_filenames = [
+    "tests/fixtures/test_input.xlsx",
+    Path("tests/fixtures/test_input.xlsx"),
+]
 
 
-@pytest.mark.parametrize('filename', fixture_input_filenames)
+@pytest.mark.parametrize("filename", fixture_input_filenames)
 def test_read_excel(filename):
     """Output should be a pandas dataframe with
-        Date : datetime64
-        Name : str 
-        Amount : Decimal
+    Date : datetime64
+    Name : str
+    Amount : Decimal
     """
-    expected = pd.DataFrame([
-        {"Date":datetime(year=2023, month=10, day=6),"Name":"Transaction 1","Amount":Decimal(-9999.00)},
-        {"Date":datetime(year=2023, month=10, day=3),"Name":"Transaction 2","Amount":Decimal(-2.00)},
-        {"Date":datetime(year=2023, month=10, day=3),"Name":"Transaction 3","Amount":Decimal(10001.00)},
-    ])
+    expected = pd.DataFrame(
+        [
+            {
+                "Date": datetime(year=2023, month=10, day=6),
+                "Name": "Transaction 1",
+                "Amount": Decimal(-9999.00),
+            },
+            {
+                "Date": datetime(year=2023, month=10, day=3),
+                "Name": "Transaction 2",
+                "Amount": Decimal(-2.00),
+            },
+            {
+                "Date": datetime(year=2023, month=10, day=3),
+                "Name": "Transaction 3",
+                "Amount": Decimal(10001.00),
+            },
+        ]
+    )
     actual = M.read_excel(filename)
     pd.testing.assert_frame_equal(expected, actual)
